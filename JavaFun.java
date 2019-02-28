6892fb5e2b9cecb75912e0cbf0f0aba6a3b67917
Vinci Allen-Steward <vinci.allensteward@gmail.com>
2019-02-27 20:56:58 -0700

    Commit HelloWorld Java

---
+++ JavaFun.java
@@ -0,0 +1,7 @@
+public class JavaFun{
+    public static void main(String[] args){
+        System.out.println("My name is Vinci Allen-Steward");
+        System.out.println("I am an unspeakble age");
+        System.out.println("I am from Viva Las Vegas");
+    }
+}
